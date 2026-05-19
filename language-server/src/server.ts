524d138365795fd1280ec48c9c2653b2cfa102ad
Gray Olson <gray@grayolson.com>
2026-05-19 13:57:23 +0200

    allow language server to be used independently of vscode extension
    move env scriptrootpaths to where it's used

--- language-server/src/server.ts
+++ language-server/src/server.ts
@@ -21,7 +21,7 @@ import {
     TypeHierarchySupertypesParams, TypeHierarchySubtypesParams,
     WorkspaceSymbol, DocumentSymbol,
     InlayHint, InlayHintParams,
-    InlineValue, InlineValueParams,
+    InlineValue, InlineValueParams, WorkspaceFolder,
 } from 'vscode-languageserver/node';
 import { TextDocument, TextDocumentContentChangeEvent } from 'vscode-languageserver-textdocument';
 
@@ -53,8 +53,14 @@ import {
     buildDisconnect, buildOpenAssets, buildCreateBlueprint
 } from './unreal-buffers';
 
-// Create a connection for the server. The connection uses Node's IPC as a transport
-let connection: Connection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+// Create a connection for the server.
+//
+// If we have a Node IPC send function available, use that, otherwise use stdio
+// to be used as a standalone lsp client.
+const ipcSendAvailble = typeof process.send === 'function';
+let connection: Connection = ipcSendAvailble
+    ? createConnection(new IPCMessageReader(process), new IPCMessageWriter(process))
+    : createConnection(process.stdin, process.stdout);
 
 // Create a connection to unreal
 let unreal : Socket;
@@ -306,6 +312,15 @@ connection.onInitialize((_params): InitializeResult => {
         }
     }
 
+    const initializationOptions = _params.initializationOptions as { additionalScriptRootFolders?: WorkspaceFolder[] } | undefined;
+
+    for (let scriptRootPath of initializationOptions.additionalScriptRootFolders) {
+        let uri = decodeURIComponent(scriptRootPath.uri);
+        if (!RootUris.includes(uri)) {
+            Roots.push(URI.parse(scriptRootPath.uri).fsPath);
+            RootUris.push(uri);
+        }
+    }
 
     connection.console.log("Workspace roots: " + Roots);
 
